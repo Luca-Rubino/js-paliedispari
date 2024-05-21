d193ef78a78c3e1c18f00c515b65e86c66e0a260
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-21 23:29:43 +0200

    Creare la funzione per invertire la parola da controllare

--- js/script.js
+++ js/script.js
@@ -3,4 +3,16 @@ console.log('Palindroma')
 // chiedo all'utente una parola
 const userWord = prompt('Inserisci la parola')
 const userWordReversed = invertiParola(userWord)
+
+// creo la mia funzione per inviertire la parola
+
+function invertiParola(userWord) {
+    let userWordReversed = ''
+  for(let i=userWord.length-1;i>=0;i--){
+    userWordReversed += userWord[i]
+  }
+  return userWordReversed
+
+}
+
 console.log('Pari e dispari')
