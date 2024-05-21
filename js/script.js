259e146bd9b89ebf2f603b702b7f459c79070b78
Luca-Rubino <lucasrubino92@gmail.com>
2024-05-21 23:27:52 +0200

    creare costanti; la prima per chiedere la parola da controllare e la seconda per avere la parola di confronto per sapere se è palindroma o meno

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log('Hi!')
+console.log('Palindroma')
+
+// chiedo all'utente una parola
+const userWord = prompt('Inserisci la parola')
+const userWordReversed = invertiParola(userWord)
+console.log('Pari e dispari')
